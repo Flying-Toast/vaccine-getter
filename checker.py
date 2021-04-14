8b291fe21fef97ec9df7f770ce23b26b184ab666
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-04-14 18:45:26 -0400

    done

---
+++ checker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from http.client import HTTPSConnection
+import json
+import time
+from string import capwords
+
+def fetch_openings():
+    headers = {
+        "Referer": "https://www.cvs.com/immunizations/covid-19-vaccine?icid=cvs-home-hero1-banner-1-link2-coronavirus-vaccine"
+    }
+    conn = HTTPSConnection("www.cvs.com")
+    conn.request("GET", "/immunizations/covid-19-vaccine.vaccine-status.ma.json?vaccineinfo", headers=headers)
+    res = conn.getresponse().read()
+    j = json.loads(res.decode("utf-8"))["responsePayloadData"]
+    ret = []
+    timestamp = j["currentTime"].split("T")[1].split(":")[:-1]
+    [hour, minute] = map(int, timestamp)
+    hour -= 9
+    timestamp = f"{hour}:{str(minute).zfill(2)}"
+    for i in j["data"]["MA"]:
+        if i["status"] != "Fully Booked":
+            ret.append(i["city"])
+    return (ret, timestamp)
+
+def main():
+    last_timestamp = ""
+    while True:
+        (openings, timestamp) = fetch_openings()
+        if last_timestamp == timestamp:
+            continue
+        print(f"Updated at {timestamp}. Openings:")
+        if len(openings) == 0:
+            print("\tNo openings")
+        else:
+            for i in openings:
+                print(f"\t{i.city}")
+        last_timestamp = timestamp
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
