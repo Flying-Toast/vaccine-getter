8fd9b81df869e9fe0791999fbb9310ea1ed59d4f
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-04-15 08:50:15 -0400

    a

--- checker.py
+++ checker.py
@@ -8,7 +8,7 @@ from smtplib import SMTP_SSL
 
 RECIPIENTS = [
     "vaccine@theschwartz.xyz",
-    "akhilibrahim@gmail.com",
+    "akhilmibrahim@gmail.com",
 ]
 
 EMAIL_ADDRESS = "vaccine-notifier@theschwartz.xyz"
@@ -26,6 +26,8 @@ def fetch_openings():
     timestamp = j["currentTime"].split("T")[1].split(":")[:-1]
     [hour, minute] = map(int, timestamp)
     hour -= 9
+    if hour < 0:
+        hour += 24
     timestamp = "%s:%s" % (hour, str(minute).zfill(2))
     for i in j["data"]["MA"]:
         if i["status"] != "Fully Booked":
